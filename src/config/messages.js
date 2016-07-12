cc8ec3f0fe4ffc7dc41fb01b841ff2846612b726
Diabeu <scibor.rudnicki@ster-project.pl>
2016-07-12 18:26:16 +0200

    feat(messages): add default messages to validationMessages
    Added max, min, minlength, maxlength, required

---
+++ src/config/messages.js
@@ -0,0 +1,22 @@
+export default {
+    max: {
+        prefix: "The max value allowed is",
+        suffix: ""
+    },
+    min: {
+        prefix: "The min value allowed is",
+        suffix: ""
+    },
+    minlength: {
+        prefix: "Number ",
+        suffix: "is the minimum length"
+    },
+    maxlength: {
+        prefix: "Number ",
+        suffix: "is the maximum length"
+    },
+    required: {
+        prefix: "This field is required",
+        suffix: ""
+    }
+};
