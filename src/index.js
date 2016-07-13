3a3d712d7ee4bea72adfec76a3df752716860548
Diabeu <scibor.rudnicki@ster-project.pl>
2016-07-13 19:15:57 +0200

    refactor(index): remove underscore

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import angular from "angular";
-import _ from "underscore";
+
+// load
 import wrappers from "./wrappers";
 import types from "./types";
+
 import validationMessages from "./config/validation-messages";
 import defaultModelAttrs from "./config/default-model-attrs";
 import defaultApiCheck from "./config/default-api-check";
 
 const moduleName = "wieldoFormlyMaterial";
 
-angular.module(moduleName, ["ngMessages", "ngMaterial", "formly"]).config((formlyConfigProvider, formlyApiCheck) => {
-    // Set wrappers
-    _.each(wrappers, (wrappers) => {
-        wrappers(formlyConfigProvider);
+angular.module(moduleName, ["ngMessages", "ngMaterial", "formly"]).config(["formlyConfigProvider","formlyApiCheck", (formlyConfigProvider, formlyApiCheck) => {
+    var load = [wrappers, types];
+
+    load.forEach((load) => {
+        for (let i = 0; i < load.length; i++) {
+            load[i](formlyConfigProvider);
+        }
     });
-    // Set types
-    _.each(types, (types) => {
-        types(formlyConfigProvider);
-    });
-    formlyConfigProvider.templateManipulators.preWrapper.push(function(template, options, scope) {
+    formlyConfigProvider.templateManipulators.preWrapper.push((template, options) => {
         defaultApiCheck(formlyApiCheck, options);
         defaultModelAttrs(options);
         return template;
     });
-}).run((formlyValidationMessages) => {
+}]).run(["formlyValidationMessages",(formlyValidationMessages) => {
     validationMessages(formlyValidationMessages);
-});
+}]);
 export default moduleName;
