8a657f79fe32b83df29ddf92bfc5e3f786388e23
Diabeu <scibor.rudnicki@ster-project.pl>
2016-07-26 18:31:11 +0200

    refactor(select): change file name from select to index, create view in other way
    Now, view is set to work with changes on options

---
+++ src/types/select/index.js
@@ -0,0 +1,38 @@
+import template from "./view.html";
+export default (formlyConfigProvider) => {
+    formlyConfigProvider.setType({
+        template: template,
+        name: "select",
+        wrapper: ["descriptionWrapper","labelWrapper","messagesWrapper","inputWrapper"],
+        defaultOptions: {
+            templateOptions: {},
+            ngModelAttrs: {
+                disabled: {
+                    attribute: "ng-disabled"
+                },
+                mdOnOpen: {
+                    statement: "md-on-open"
+                },
+                mdOnClose: {
+                    statement: "md-on-close"
+                },
+                mdContainerClass: {
+                    attribute: "md-container-class"
+                },
+                ngRequired: {
+                    bound: "ng-required"
+                }
+            }
+        },
+        apiCheck: (check) => ({
+            templateOptions: {
+                groups: check.arrayOf(check.object).optional,
+                mdContainerClass: check.string.optional,
+                mdOnOpen: check.func.optional,
+                mdOnClose: check.func.optional,
+                options: check.arrayOf(check.object).optional,
+                required: check.bool.optional
+            }
+        })
+    });
+};
