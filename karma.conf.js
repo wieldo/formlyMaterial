7940b65a54723558ac2e5ab56142158a6d6009c2
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-16 16:10:23 +0200

    chore: add chrome test

--- karma.conf.js
+++ karma.conf.js
@@ -76,10 +76,18 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
+    // This is the new content for your travis-ci configuration test
+    //  Custom launcher for Travis-CI
+    customLaunchers: {
+          Chrome_travis_ci: {
+              base: 'Chrome',
+              flags: ['--no-sandbox']
+          }
+      },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
