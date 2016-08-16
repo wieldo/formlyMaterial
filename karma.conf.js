52a7f2f8bb66e4dd84d50141df2dd735e77241c7
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-16 16:15:01 +0200

    chore: remove chrome test

--- karma.conf.js
+++ karma.conf.js
@@ -74,16 +74,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+    browsers: ['Firefox', 'PhantomJS'],
 
-    // This is the new content for your travis-ci configuration test
-    //  Custom launcher for Travis-CI
-    customLaunchers: {
-          Chrome_travis_ci: {
-              base: 'Chrome',
-              flags: ['--no-sandbox']
-          }
-      },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
