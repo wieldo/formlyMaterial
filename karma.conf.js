8d63a4753021c24a172917213f1e4855e6d03a39
Diabeu <scibor.rudnicki@ster-project.pl>
2016-08-16 14:46:58 +0200

    chore: add karma config

---
+++ karma.conf.js
@@ -0,0 +1,88 @@
+// Karma configuration
+// Generated on Fri Jul 29 2016 14:59:48 GMT+0200 (CEST)
+
+// Note that you ".babelrc" will be the default options for babel.
+var babelMoreOptions = { presets: 'es2015' };
+
+require('argv-set-env')();
+var webpack = require('./tools/webpack')('test');
+
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      "node_modules/jquery/dist/jquery.js",
+      "node_modules/api-check/dist/api-check.js",
+      "node_modules/angular/angular.js",
+      "node_modules/angular-mocks/angular-mocks.js",
+      "node_modules/angular-animate/angular-animate.js",
+      "node_modules/angular-aria/angular-aria.js",
+      "node_modules/angular-messages/angular-messages.js",
+      "node_modules/angular-material/angular-material.js",
+      "node_modules/angular-material/angular-material-mocks.js",
+      "node_modules/angular-formly/dist/formly.js",
+      "node_modules/underscore/underscore.js",
+      "test/index.js"
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      "test/index.js": ["webpack"]
+    },
+
+    webpack: webpack,
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress', "coverage"],
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
